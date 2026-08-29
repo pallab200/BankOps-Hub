1b1b32a0ad067dd46b5438d6342f2aad9bf69d7e
Pallab <tapallab00@gmail.com>
2026-08-29 20:20:20 +0600

    Add Subordinated Bond form editor with assets and configuration

---
+++ Subordinated Bond/subbond_auto_import.js
@@ -0,0 +1,295 @@
+window.SUBBOND_AUTO_IMPORT_DATA = {
+  "exportedAt": "2026-08-27T17:18:05.924Z",
+  "form": {
+    "fullNameEn": "",
+    "fullNameBn": "",
+    "occupation": "",
+    "nid": "",
+    "passportNo": "",
+    "etin": "",
+    "tradeLicenseNo": "",
+    "businessRegNo": "",
+    "mobile": "",
+    "email": "",
+    "isMinor": "no",
+    "presentAddress": "",
+    "permanentAddress": "",
+    "totalAmount": "",
+    "amountInWords": "",
+    "sourceOfFund": "",
+    "effectiveDate": "",
+    "bankName": "",
+    "branchName": "",
+    "bankAccountNumber": "",
+    "routingNumber": "",
+    "altPaymentMode": "",
+    "nomineeName": "",
+    "nomineeRelation": "",
+    "nomineeDob": "",
+    "nomineeSharePercent": "100",
+    "nomineeNid": "",
+    "nomineeMobile": "",
+    "nomineeAddress": "",
+    "nomineePermanentAddress": "",
+    "hasNominee2": "no",
+    "nominee2_name": "",
+    "nominee2_relation": "",
+    "nominee2_dob": "",
+    "nominee2_sharePercent": "",
+    "nominee2_nid": "",
+    "nominee2_mobile": "",
+    "nominee2_address": "",
+    "nominee2_permanentAddress": "",
+    "guardianName": "",
+    "guardianRelation": "",
+    "guardianNid": "",
+    "guardianMobile": "",
+    "guardianAddress": "",
+    "guardianPermanentAddress": "",
+    "officerName": "",
+    "rmCode": "",
+    "todayDate": "",
+    "branchRef": "",
+    "comments": "",
+    "branchNameOfficial": "",
+    "branchCode": "",
+    "sameAsPresent": "",
+    "nomineeSameAddress": "",
+    "nominee2SameAddress": "",
+    "guardianSameAddress": ""
+  },
+  "positions": {
+    "1::fullNameBn": {
+      "top": "29.8289%",
+      "left": "27.4959%"
+    },
+    "1::fullNameEn": {
+      "top": "37.4358%",
+      "left": "37.9024%"
+    },
+    "1::nid": {
+      "top": "44.945%",
+      "left": "39.5935%"
+    },
+    "1::passportNo": {
+      "top": "48.5606%",
+      "left": "44.7805%"
+    },
+    "1::occupation": {
+      "top": "52.4485%",
+      "left": "38.935%"
+    },
+    "1::tradeLicenseNo": {
+      "top": "56.0915%",
+      "left": "49.0732%"
+    },
+    "1::businessRegNo": {
+      "top": "59.707%",
+      "left": "36.2439%"
+    },
+    "1::presentAddress": {
+      "top": "62.7351%",
+      "left": "13.4878%"
+    },
+    "1::permanentAddress": {
+      "top": "70.3447%",
+      "left": "13.7398%"
+    },
+    "1::etin": {
+      "top": "78.3688%",
+      "left": "23.7398%"
+    },
+    "1::mobile": {
+      "top": "82.236%",
+      "left": "27.2683%"
+    },
+    "1::email": {
+      "top": "85.7605%",
+      "left": "19.4146%"
+    },
+    "2::totalAmount": {
+      "top": "13.2112%",
+      "left": "38%"
+    },
+    "2::amountInWords": {
+      "top": "16.1368%",
+      "left": "36.8618%"
+    },
+    "2::payCash": {
+      "top": "20.1595%",
+      "left": "30.6496%"
+    },
+    "2::payCheque": {
+      "top": "20.1021%",
+      "left": "41.7935%"
+    },
+    "2::payTransfer": {
+      "top": "20.0446%",
+      "left": "55.6813%"
+    },
+    "2::sourceOfFund": {
+      "top": "23.6498%",
+      "left": "25.1707%"
+    },
+    "2::bankName": {
+      "top": "34.778%",
+      "left": "24.9024%"
+    },
+    "2::branchName": {
+      "top": "38.5533%",
+      "left": "24.1707%"
+    },
+    "2::bankAccountNumber": {
+      "top": "42.3562%",
+      "left": "26.6585%"
+    },
+    "2::routingNumber": {
+      "top": "45.8741%",
+      "left": "26.3333%"
+    },
+    "2::altPaymentMode": {
+      "top": "49.6645%",
+      "left": "50.8618%"
+    },
+    "2::nomineeName": {
+      "top": "62.8843%",
+      "left": "28.0406%"
+    },
+    "2::nomineeRelation": {
+      "top": "66.5819%",
+      "left": "36.2113%"
+    },
+    "2::nomineeSharePercent": {
+      "top": "70.3944%",
+      "left": "32.065%"
+    },
+    "2::nomineeNid": {
+      "top": "74.1496%",
+      "left": "39.5935%"
+    },
+    "2::nomineeMobile": {
+      "top": "77.9166%",
+      "left": "25.7642%"
+    },
+    "2::nomineeAddress": {
+      "top": "80.8089%",
+      "left": "12.309%"
+    },
+    "2::nomineePermanentAddress": {
+      "top": "84.7108%",
+      "left": "12.6342%"
+    },
+    "3::nominee2_name": {
+      "top": "10.4759%",
+      "left": "27.8537%"
+    },
+    "3::nominee2_relation": {
+      "top": "14.1363%",
+      "left": "36.374%"
+    },
+    "3::nominee2_sharePercent": {
+      "top": "17.9117%",
+      "left": "31.9837%"
+    },
+    "3::nominee2_nid": {
+      "top": "21.5721%",
+      "left": "39.5935%"
+    },
+    "3::nominee2_mobile": {
+      "top": "25.3475%",
+      "left": "25.5772%"
+    },
+    "3::nominee2_address": {
+      "top": "28.3982%",
+      "left": "12.3171%"
+    },
+    "3::nominee2_permanentAddress": {
+      "top": "35.8808%",
+      "left": "12.6342%"
+    },
+    "3::guardianName": {
+      "top": "50.219%",
+      "left": "18.7398%"
+    },
+    "3::guardianRelation": {
+      "top": "53.9094%",
+      "left": "36.4553%"
+    },
+    "3::guardianNid": {
+      "top": "57.8722%",
+      "left": "32.9756%"
+    },
+    "3::guardianMobile": {
+      "top": "61.5625%",
+      "left": "25.4472%"
+    },
+    "3::guardianAddress": {
+      "top": "64.5907%",
+      "left": "12.1544%"
+    },
+    "3::guardianPermanentAddress": {
+      "top": "70.7803%",
+      "left": "12.3903%"
+    },
+    "4::branchNameOfficial": {
+      "top": "36.7257%",
+      "left": "24.2276%"
+    },
+    "4::officerName": {
+      "top": "41.2137%",
+      "left": "23.8537%"
+    },
+    "4::rmCode": {
+      "top": "45.9891%",
+      "left": "21.0894%"
+    },
+    "4::comments": {
+      "top": "49.8447%",
+      "left": "28.3333%"
+    },
+    "4::officer1Seal": {
+      "top": "66.4211%",
+      "left": "7.33333%"
+    },
+    "4::officer2Seal": {
+      "top": "66.421%",
+      "left": "34.3902%"
+    },
+    "4::officer3Seal": {
+      "top": "66.5359%",
+      "left": "64.0976%"
+    },
+    "10::fullNameEn": {
+      "top": "41.0091%",
+      "left": "60.9431%"
+    },
+    "10::totalAmount": {
+      "top": "44.6987%",
+      "left": "52.5935%"
+    },
+    "10::amountInWords": {
+      "top": "48.4607%",
+      "left": "37.7155%"
+    },
+    "10::effectiveDate": {
+      "top": "52.1502%",
+      "left": "57.4147%"
+    },
+    "10::officer2Seal": {
+      "top": "85.4198%",
+      "left": "25.0406%"
+    },
+    "10::officer3Seal": {
+      "top": "85.1897%",
+      "left": "74.1789%"
+    },
+    "10::branchCode": {
+      "top": "87.2614%",
+      "left": "24.3089%"
+    },
+    "10::branchRef": {
+      "top": "88.8234%",
+      "left": "44.7154%"
+    }
+  }
+};
---
+++ Subordinated Bond/subbond_config.js
@@ -0,0 +1,15 @@
+/**
+ * ===================================
+ *  SUBORDINATED BOND - CONFIGURATION
+ * ===================================
+ * 
+ * Show Developer Toggle:
+ *   true  = Developer toggle button visible in the side menu
+ *   false = Developer toggle button hidden (normal users won't see it)
+ * 
+ * Just change the value below and refresh the app.
+ */
+
+window.SUBBOND_CONFIG = {
+  showDeveloperToggle: false
+};
